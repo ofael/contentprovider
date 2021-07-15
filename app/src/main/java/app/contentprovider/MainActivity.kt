6d1f23a90ae1f8d652784b8a0be34ca2ffc31faf
ofael <fael_assis@hotmail.com>
2021-07-15 12:28:33 -0300

    Creating Coordinator Layout and implementing Card View with constrain layout

--- app/src/main/java/app/contentprovider/MainActivity.kt
+++ app/src/main/java/app/contentprovider/MainActivity.kt
@@ -1,11 +1,52 @@
 package app.contentprovider
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import app.contentprovider.database.NotesDataBaseHelper.Companion.TITLE_NOTES
+import app.contentprovider.database.NotesProvider.Companion.URI_NOTES
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+//Precisamos instanciar o LoaderManager, faz a busca em segundo plano no cursor
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+    lateinit var notesRecycleView : RecyclerView
+    lateinit var noteAdd : FloatingActionButton
+
+    lateinit var adapter : NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteAdd = findViewById(R.id.note_add)
+        noteAdd.setOnClickListener{
+
+        }
+
+        adapter = NotesAdapter()
+        adapter.setHasStableIds(true)
+
+        notesRecycleView = findViewById(R.id.notes_recycle)
+        notesRecycleView.layoutManager = LinearLayoutManager(this)
+        notesRecycleView.adapter = adapter
+    }
+    //instanciar aquilo que será buscado
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
+        CursorLoader(this,URI_NOTES, null, null, null, TITLE_NOTES)
+
+    //pegar os dados recebidos do on creatloader e manipular
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if(data != null){
+
+        }
+    }
+    //acabar com a pesquisa em segundo plano no LoaderManager
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
