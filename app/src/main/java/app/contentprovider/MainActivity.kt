5a3e0c20736bf1ca1cf6264cb64f3907a38ce0bf
ofael <fael_assis@hotmail.com>
2021-07-15 14:06:29 -0300

    Complementing MainActivity.kt and creating the class NoteDetailsFragments.kt

--- app/src/main/java/app/contentprovider/MainActivity.kt
+++ app/src/main/java/app/contentprovider/MainActivity.kt
@@ -1,8 +1,10 @@
 package app.contentprovider
 
 import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BaseColumns._ID
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
@@ -28,7 +30,16 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
 
         }
 
-        adapter = NotesAdapter()
+        adapter = NotesAdapter(object : NoteClickListener{
+            override fun noteClickItem(cursor: Cursor) {
+                val id: Long = cursor.getLong(cursor.getColumnIndex(_ID))
+            }
+
+            override fun noteRemoveItem(cursor: Cursor?) {
+                val id : Long? = cursor?.getLong((cursor.getColumnIndex(_ID)))
+                contentResolver.delete(Uri.withAppendedPath(URI_NOTES, id.toString()), null, null)
+            }
+        })
         adapter.setHasStableIds(true)
 
         notesRecycleView = findViewById(R.id.notes_recycle)
---
+++ app/src/main/java/app/contentprovider/NotesDetailsFragment.kt
@@ -0,0 +1,42 @@
+package app.contentprovider
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+
+class NotesDetailsFragment : DialogFragment(), DialogInterface.OnClickListener {
+
+    private lateinit var noteEditTitle : EditText
+    private lateinit var noteEditDescription : EditText
+    private var id : Long = 0
+
+    companion object{
+        private const val EXTRA_ID = "id"
+        fun newInstance(id : Long) : NotesDetailsFragment{
+            val bundle = Bundle()
+            bundle.putLong(EXTRA_ID, id)
+
+            val notesFragment = NotesDetailsFragment()
+            notesFragment.arguments = bundle
+
+            return notesFragment
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view: View? = activity?.layoutInflater?.inflate(R.layout.note_detail, null)
+
+        noteEditTitle = view?.findViewById(R.id.note_edt_title) as EditText
+        noteEditDescription = view?.findViewById(R.id.note_edt_description) as EditText
+
+        return super.onCreateDialog(savedInstanceState)
+    }
+
+    override fun onClick(dialog: DialogInterface?, which: Int) {
+        TODO("Not yet implemented")
+    }
+}
